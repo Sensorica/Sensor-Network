a64dc973fcd3110bcd2d8b9410a536535542e4b2
Fabio Balli <info@fabioballi.net>
2015-12-14 15:48:00 -0500

    ...
    ...

---
+++ Sensor Tests/Individual_Sensor_Sketches/Optical limit switch/optical_limit_switch_preliminary_test/optical_limit_switch_preliminary_analog_test.ino
@@ -0,0 +1,23 @@
+/*
+Demonstrate IR interrupt opto switch with analog in, for threshold adjustment
+Author: T!b!
+Date: 2015-11-25
+*/
+int analogPin = 3;     // potentiometer wiper (middle terminal) connected to analog pin 3
+                       // outside leads to ground and +5V
+int val = 0;     // variable to store the value read
+int LEDPin = 13; // output
+
+void setup()                    // run once, when the sketch starts
+{
+  Serial.begin(9600);          //  setup serial
+  pinMode(LEDPin, OUTPUT);      // sets the digital pin as output
+}
+
+void loop()                     // run over and over again
+{
+   val = analogRead(analogPin);    // read the input pin
+   Serial.println(val);       // debug value
+   delay(300);
+}
+
