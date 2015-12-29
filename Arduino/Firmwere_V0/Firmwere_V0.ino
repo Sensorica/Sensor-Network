be39cc618b1e8cd5778f3859dc39ed883611c0cc
TiberiusB <tiberius.brastaviceanu@gmail.com>
2015-12-29 11:40:51 -0500

    ...

--- Arduino/Firmwere_V0/Firmwere_V0.ino
+++ Arduino/Firmwere_V0/Firmwere_V0.ino
@@ -1,22 +1,42 @@
+/* First number is the pin, second is time to poll for and third is polling frequency, both in milliseconds.
+Still need to add reporting to serial and some error checking (is sensor connected?) */
+ 
+AnalogSensor shaftTemp(10, 1, 500);
+AnalogSensor ambientTemp(11, 1, 1000);
+AnalogSensor packingTemp(12, 1, 100);
+DigitalSensor dripSensor(14, 1, 10); 
+
+void setup()
+{
+} 
+void loop()
+{
+  shaftTemp.Update();
+  ambientTemp.Update();
+  packingTemp.Update();
+  dripSensor.Update();
+}
+
+
 class DripSensor
 {
-	// Class Member Variables
-	// These are initialized at startup
-	int sensorPin;      // the number of the sensor pin
-	long DelayTime;
+  // Class Member Variables
+  // These are initialized at startup
+  int sensorPin;      // the number of the sensor pin
+  long DelayTime;
  
-	// These maintain the current state
-	int previousValue; // sensorData used to store the sensor data
-        int currentValue; // sensorData used to store the sensor data
-	unsigned long previousMillis;  // will store last time sensorData was updated
+  // These maintain the current state
+  int previousValue; 
+        int currentValue;// sensorData used to store the sensor data
+  unsigned long previousMillis;  // will store last time sensorData was updated
         int drip;
 public:
                DripSensor(int pin, long frequency)
                  {
-	          sensorPin = pin;
-	          DelayTime = frequency;
-	          previousValue = 0;
-	          previousMillis = 0;
+            sensorPin = pin;
+            DelayTime = frequency;
+            previousValue = 0;
+            previousMillis = 0;
                   drip=1;       
                  }
 void Update()
@@ -26,7 +46,7 @@ void Update()
    
             if (previousMillis >= DelayTime)
                 {
-   	        currentValue = analogRead(sensorPin);    // read the input pin
+            currentValue = analogRead(sensorPin);    // read the input pin
    
                  if ((previousValue < 150) && (currentValue > 250))
                      { 
@@ -35,7 +55,7 @@ void Update()
                      drip += 1; 
                      }   
                   previousValue = currentValue; 
-                  }
+                 }
                 previousMillis = currentMillis - previousMillis;  // Remember the time
              }
             
@@ -50,13 +70,7 @@ void setup()
 } 
 void loop()
 {
-  long a;
-  long b;
-  a = millis();
-  
-	dripSensor.Update();
-  b = millis();
-  Serial.println(b-a);
-  delay(200);
+  dripSensor.Update();
 }
+
 
