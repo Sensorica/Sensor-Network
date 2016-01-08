db381a9f492364f2e0dbb4988a6ad8810f29386e
snowy13 <jim.anastassiou@gmail.com>
2016-01-07 22:50:52 -0500

    Added drop counter ISR with schmitt trigger

---
+++ Arduino/Drop_sensor_test/Drop_counter_ISR_schmitt_trigger/drop_sensor_ISR_schmitt_trigger/drop_sensor_ISR_schmitt_trigger.ino
@@ -0,0 +1,27 @@
+const byte LED = 13;
+const byte BUTTON = 2;
+volatile boolean drop_occured = false;
+int drop_count = 0;
+
+// Interrupt Service Routine (ISR)
+void drop ()
+{
+  drop_occured = true;
+}  
+
+void setup ()
+{
+  Serial.begin(115200);
+  pinMode (LED, OUTPUT);  // so we can update the LED
+  digitalWrite (BUTTON, HIGH);  // internal pull-up resistor built in :-)
+  attachInterrupt (0, drop, RISING);  // attach interrupt handler
+}  // end of setup
+
+void loop ()
+{
+  if (drop_occured) {
+    drop_count += 1;
+    drop_occured = false;
+    Serial.println (drop_count);
+  }    
+} 
