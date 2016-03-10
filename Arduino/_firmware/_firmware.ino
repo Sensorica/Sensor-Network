3810d7aed6adb820a08a586d1cfb73f97685b8d8
Scott Laughlin <scott.laughlin15@gmail.com>
2016-03-10 16:37:51 -0500

    Integrated the level sensor, drain logic, and flow rate calculations. - Still needs to be tested - Needs to be merged with the other branch

--- Arduino/_firmware/_firmware.ino
+++ Arduino/_firmware/_firmware.ino
@@ -36,28 +36,59 @@ void revolution ()
 {
   revolution_occured = true;
 }  
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//                                                  Drip Sensor
-const byte SENSOR = 3;
-volatile boolean drop_occured = false;
-int drop_count = 0;
-unsigned long drop_durations[3] = {0,0,0};
-unsigned int drop_rate;
-unsigned long last_drop_time;
 
-// Drip sensor Interrupt Service Routine (ISR)
-void drop ()
-{
-  drop_occured = true;
-} 
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////// 
+//                                                 LOAD WASHER
 
+int loadWasherPin = A0;
+int analog_load_value = 0;
+double load_in_lbs = 0;
+
+//Insert Calibration Data Here: Level = AX + B format (rounded to the nearest integer N)
+//X is the applied weight in lbs
+//A is our linear rate in Arduino ADC levels / lbs
+//B is our reference level as applied by the amplifyer circuit
+double A_linear_rate = 0.01;
+double B_reference_level = 483.854;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////// 
+//                                                LEVEL SENSOR
+
+boolean initialize = true;
+int levelSensorPin = A1;
+int analog_water_level = 0;
+double water_level_cm = 0;
+double flow_calc_water_level_cm = 0;
+unsigned long calc_time_ms = 0;
+
+double vesicle_diameter_cm = 11.43;//4.5 inch x 2.54 cm / inch
+double vesicle_area_cm_2 = (3.14159) * (vesicle_diameter_cm / 2) * (vesicle_diameter_cm / 2);
+
+//Insert Calibration Data Here: Level = AX + B format (rounded to the nearest integer N)
+double A_water_rate = 11.96;
+double B_water_reference_level = 327.45;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////// 
+//                                                SOLENOID DRAIN VALVE
+
+
+int solenoidValvePin = 14; //Pin controlling the valve
+int drain_at_cm_level = 5; 
+int close_valve_at_cm_level = 1;
+
+boolean solenoid_valve_open = false;
+double flow_rate_cc_per_min = 0;
+double water_level_buffer; //The water level buffer is how much the water level should increase before calculating a new flow rate
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////// 
 
 void setup()
 {
   Serial.begin(9600);   //Enable serial at high speed 
   
   // Tachometer setup
+  pinMode(solenoidValvePin,OUTPUT);
   digitalWrite (TACH, HIGH);  // internal pull-up resistor built in :-)
   attachInterrupt (0, revolution, RISING);  // attach interrupt handler for tachometer
   
@@ -71,10 +102,6 @@ void setup()
   sensors.requestTemperatures();
   delayInMillis = 750 / (1 << (3)); 
   lastTempRequest = millis(); 
-  
-  // Drip sensor setup
-  digitalWrite (SENSOR, HIGH);  // internal pull-up resistor built in :-)
-  attachInterrupt (1, drop, RISING);  // attach interrupt handler  
 } 
 
 
@@ -105,18 +132,52 @@ void loop()
     delayInMillis = 750 / (1 << (3));
     lastTempRequest = millis(); 
   }
-  
-  ///////////////////////////////////////////////////////Drops
-  
-  if (drop_occured) {
-    drop_count += 1;
-    drop_durations[drop_count -1] = millis() - last_drop_time;
-    last_drop_time = millis();
-    if (drop_count = 3) drop_count = 0;    
-    drop_rate = 60000 / (drop_durations[0] + drop_durations [1] + drop_durations [2]);
-    drop_occured = false;
-    }    
-  
+
+
+//////////////////////////////////////////////////////Load Washer
+ analog_load_value = analogRead(loadWasherPin);
+ load_in_lbs = ( analog_load_value - B_reference_level ) / A_linear_rate;
+
+ //TODO: Send this value to the cloud for storage and analytics
+
+
+ //////////////////////////////////////////////////////Water Level Sensor
+ 
+ analog_water_level = analogRead(levelSensorPin);
+ water_level_cm = ( analog_water_level - B_water_reference_level ) / A_water_rate;
+
+if(initialize==true){
+  flow_calc_water_level_cm = water_level_cm;
+  calc_time_ms = millis();
+  initialize==false;
+}
+
+ 
+
+ //////////////////////////////////////////////////////Solenoid Drain Valve
+
+ //Start the drain if the water level is too high
+ if(water_level_cm >= drain_at_cm_level){
+  //TODO: If water level is way too high, then water is accumulating so send the operator a warning message
+  digitalWrite(solenoidValvePin,HIGH);
+  solenoid_valve_open = true;
+ }
+
+ if (solenoid_valve_open == false){
+  if (water_level_cm >= (flow_calc_water_level_cm + water_level_buffer)){
+   flow_rate_cc_per_min = ( analog_water_level - flow_calc_water_level_cm ) / (millis() - calc_time_ms);
+   flow_calc_water_level_cm = analog_water_level;
+  }
+ }
+
+  if (water_level_cm <= close_valve_at_cm_level){
+  digitalWrite(solenoidValvePin,LOW);
+  solenoid_valve_open = false;
+  initialize=true;
+ }
+
+
+
   //////////////////////////////////////////////////////Printing
   
   if (millis() - last_print_time >= PRINT_DELAY){
@@ -125,9 +186,13 @@ void loop()
     data += ",";
     data += casing_temperature;
     data += ",";
-    data += drop_rate;
-    data += ",";
     data += rpm;
+    data += ",";
+    data += load_in_lbs;
+    data += ",";
+    data += water_level_cm;
+    data += ",";
+    data += flow_rate_cc_per_min;
     
     Serial.println (data);
     
