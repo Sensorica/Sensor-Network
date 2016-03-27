1b5ecfc9ff2308c14b762568efd39f4a15a283ab
Scott Laughlin <scott.laughlin15@gmail.com>
2016-03-26 23:26:24 -0400

    Testing .gitignore and the creation of a config_file.py file that is not uploaded to git. This will be used to store API keys that we don't want to share publicly on git.

---
+++ export_to_csv/s3upload.py
@@ -0,0 +1,24 @@
+import boto.s3
+import config_file
+
+
+conn = boto.connect_s3(aws_access_key_id=config_file.AWS_ACCESS_KEY,
+                       aws_secret_access_key=config_file.AWS_SECRET_KEY)
+
+def percent_cb(complete,total):
+    print ('.')
+
+def upload_to_s3_bucket_path(bucketname,path,filename):
+    mybucket = conn.get_bucket(bucketname)
+    fullkeyname=os.path.join(path,filename)
+    key = mybucket.new_key(fullkeyname)
+    key.set_contents_from_filename(filename,cb=percent_cb,num_cb=10)
+
+def upload_to_s3_bucket_root(bucketname,filename):
+    mybucket = conn.get_bucket(bucketname)
+    key = mybucket.new_key(filename)
+    key.set_contents_from_filename(filename,cb=percent_cb,num_cb=10)
+
+upload_to_s3_bucket_root('mltest12345','banking-batch.csv')
+
+
