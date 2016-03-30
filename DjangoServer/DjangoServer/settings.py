768cf7341f17258c2e4a901665e71ef40d68933e
Scott Laughlin <scott.laughlin15@gmail.com>
2016-03-30 13:43:20 -0400

    Added sponsor's defined metadata to the admin panels

--- DjangoServer/DjangoServer/settings.py
+++ DjangoServer/DjangoServer/settings.py
@@ -119,13 +119,16 @@ USE_L10N = True
 USE_TZ = True
 
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = BASE_DIR
+STATIC_ROOT = 'static/'
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    ('assets',os.path.join(BASE_DIR, 'static'))
-
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static')
+# )
