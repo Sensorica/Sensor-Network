521e92c7bd215b2a762421c819a5300956ee4d98
Scott Laughlin <scott.laughlin15@gmail.com>
2016-04-28 11:43:04 -0400

    FFT is now working, and the frequency range can be modified by changing the down_sampling_rate parameter

--- Arduino/_nano_firmware/nanoFirmware2/nanoFirmware2.ino
+++ Arduino/_nano_firmware/nanoFirmware2/nanoFirmware2.ino
@@ -10,7 +10,8 @@ visualizing the data.
 
 // do #defines BEFORE #includes
 #define LOG_OUT 1 // use the log output function
-#define FFT_N 128 // set to 256 point fft
+#define FFT_N 256 // set to 256 point fft
+#define WINDOW 1 // Enable windowing
 
 #include <FFT.h> // include the library
 //#include <prescaler.h>
@@ -32,9 +33,29 @@ int digitalSleepPin = 3;
 String data;
 
 int sampling_freq_hz = 200;
+int down_sampling_rate = 20;
+int down_sampling_counter = 0;
 int millisecond_delay = 16; //This should be the same as 1000 / 200;
 int microsecond_delay = millisecond_delay * 1000;
-//int sampling_period_ms = 1000 / sampling_freq_hz;
+
+// Define various ADC prescalers
+//Initialization flags
+const unsigned char PS_2_i = 0xe1;
+const unsigned char PS_4_i = 0xe2;
+const unsigned char PS_8_i = 0xe3;
+const unsigned char PS_16_i = 0xe4;
+const unsigned char PS_32_i = 0xe5; //Chosen Prescaler
+const unsigned char PS_64_i = 0xe6;
+const unsigned char PS_128_i = 0xe7;
+
+//Code flags -> should use the same prescaler as the initialization
+const unsigned char PS_2_c = 0xf1;
+const unsigned char PS_4_c = 0xf2;
+const unsigned char PS_8_c = 0xf3;
+const unsigned char PS_16_c = 0xf4;
+const unsigned char PS_32_c = 0xf5; // Chosen Prescaler
+const unsigned char PS_64_c = 0xf6;
+const unsigned char PS_128_c = 0xf7;
 
 void setup() {
     // declare the ledPin as an OUTPUT:
@@ -45,8 +66,9 @@ void setup() {
   digitalWrite(digitalSelectPin, HIGH); //Set to HIGH for 6G sensitivity, OR LOW for 1.5G sensitivity
   digitalWrite(digitalSleepPin, HIGH);
   Serial.begin(115200); // use the serial port
-  TIMSK0 = 0; // turn off timer0 for lower jitter - delay() and millis() killed
-  ADCSRA = 0xe7; // set the adc to free running mode // This is the default -> 229 or 0xe5
+  TIMSK0 = 0; //Reduces jitter
+  ADCSRA = PS_32_i; //HERE!!!!!!
+  
   ADMUX = 0x40; // use adc0
   DIDR0 = 0x01; // turn off the digital input for adc0
 }
@@ -58,37 +80,47 @@ void loop() {
 //  sensorY = analogRead(sensorY_pin);
 //  sensorX = analogRead(sensorX_pin);
     cli();  // UDRE interrupt slows this way down on arduino1.0
-    for (int i = 0 ; i < 2*FFT_N; i += 2) { // save 256 samples
+    for (int i = 0 ; i < 2 * FFT_N; i += 2) { // save 256 samples
       while(!(ADCSRA & 0x10)); // wait for adc to be ready -> Checking bit 4
-      ADCSRA = 0xf7; // restart adc
+      
+      ADCSRA = PS_32_c; // restart adc // HERE!!!!!!
+      
       byte m = ADCL; // fetch adc data
       byte j = ADCH;
       int k = (j << 8) | m; // form into an int
       k -= 0x0200; // form into a signed int
       k <<= 6; // form into a 16b signed int
+
+      if(down_sampling_counter % down_sampling_rate == 0)
+      {
       fft_input[i] = k; // put real data into even bins
       fft_input[i+1] = 0; // set odd bins to 0
-//      delay(millisecond_delay); //How many milisecconds between samples
-//      delayMicroseconds(2000);
-//    delay(millisecond_delay);
+      } else{
+        i-=2;
+      }
+      down_sampling_counter++;
+//      delay(1);
     }
+    
     // window data, then reorder, then run, then take output
     fft_window(); // window the data for better frequency response
     fft_reorder(); // reorder the data before doing the fft
     fft_run(); // process the data in the fft
     fft_mag_log(); // take the output of the fft
     sei(); // turn interrupts back on
+    
     Serial.println("Fourier Transform:");
-    for(int i = 0 ; i < FFT_N; i++){
+    for(int i = 0 ; i < FFT_N/2; i++){
       Serial.print(fft_log_out[i]);
       Serial.print(",");
-//      if(i%100==99){
-//        Serial.println("");
-//      }
     }
     Serial.println("");
 
+//delay(2000);
+
+//When TIMSK0 is on: 
 delay(120000);
+
 //    delay(500000); 
   }
 //  if (millis() - lastPrintTime >= delayInMillis) {
